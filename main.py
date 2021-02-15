0f44e7fdd9f2ae53122ac1ef02b5efd83ab38adc
n-rule <rule@ukr.net>
2021-02-15 22:54:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = '9fb60369241b9d962759b2e7c4db10bc'
+ONE_CALL_API = 'https://api.openweathermap.org/data/2.5/onecall?'
+LAT = 50.447731
+LON = 30.542721
+params = {
+    'lat': LAT,
+    'lon': LON,
+    'appid': API_KEY,
+    'exclude': 'current,minutely,daily'
+}
+
+pull = requests.get(ONE_CALL_API, params)
+pull.raise_for_status()
+data = pull.json()
+print(data)
+
+
+
+# lat={lat}&lon={lon}&exclude={part}&appid={API key}
